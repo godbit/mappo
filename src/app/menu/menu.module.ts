3433d2d8724e638f86338d517992b2fa16a2537b
godbit <emil.plynning@gmail.com>
2019-01-05 11:20:18 +0100

    add search base

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
 import { MenuContainerComponent } from './menu-container/menu-container.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [NavComponent, NavItemComponent, MenuContainerComponent],
+  declarations: [NavComponent, NavItemComponent, MenuContainerComponent, SearchComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    NavComponent,
     MenuContainerComponent
   ]
 
