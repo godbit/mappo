7d7ee7764cac966b3840c56c0e869a4c06cf8922
godbit <emil.plynning@gmail.com>
2019-01-05 01:30:45 +0100

    remove multi nav-item

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
-import { NavItemMultiComponent } from './nav-item-multi/nav-item-multi.component';
 
 @NgModule({
-  declarations: [NavComponent, NavItemComponent, NavItemMultiComponent],
+  declarations: [NavComponent, NavItemComponent],
   imports: [
     CommonModule
   ],
