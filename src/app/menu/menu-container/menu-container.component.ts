887c5e5b6a3ac2052e8af00128113d88d0fb4315
godbit <emil.plynning@gmail.com>
2019-01-05 10:04:30 +0100

    add menu-container component

---
+++ src/app/menu/menu-container/menu-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-container',
+  templateUrl: './menu-container.component.html',
+  styleUrls: ['./menu-container.component.css']
+})
+export class MenuContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { NavItemComponent } from './nav-item/nav-item.component';
+import { MenuContainerComponent } from './menu-container/menu-container.component';
 
 @NgModule({
-  declarations: [NavComponent, NavItemComponent],
+  declarations: [NavComponent, NavItemComponent, MenuContainerComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    NavComponent
+    NavComponent,
+    MenuContainerComponent
   ]
 
 })
